75893514a32dc4c39a19887999ff344a6bb698b4
Amandeep <aman.01298@gmail.com>
2018-07-04 02:28:06 +0530

    xyz

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Go to Hell Ashu!'))
+app.get('/', (req, res) => res.send('Go to Hell Aman! Also screw you'))
 
 app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
