587a32de4246581144e48ef5ee092b25556df01b
Amandeep <aman.01298@gmail.com>
2018-07-04 23:16:34 +0530

    abc

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var path = require('path')
 
-app.get('/', (req, res) => res.send('Wello World'))
 
-app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/index.html')));
+
+app.get('/song', (req, res) => res.sendFile(path.join(__dirname + '/song.html')));
+
+app.listen(process.env.PORT||3010, () => console.log('Example app listening on port 3000!'));
